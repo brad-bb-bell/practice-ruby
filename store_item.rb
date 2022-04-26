5abd5db251d9917cba851084928f6f06a4ad45f6
Brad Bell <brad.bb.bell@gmail.com>
2022-04-26 13:09:44 -0600

    Added a puts command

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 item1 = {name: "doughnut", price: 1, flavor: "glazed"}
 item2 = {name: "muffin", price: 2, flavor: "lemon poppyseed"}
 item3 = {name: "pizza", price: 3, flavor: "pepperoni"}
+
+puts "My store has great food. #{item1[:name]}, #{item2[:name]} and #{item3[:name]}."
