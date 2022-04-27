c554db40628c448d6012f451db5134687cc4565a
Brad Bell <brad.bb.bell@gmail.com>
2022-04-27 12:14:06 -0600

    Added instance variables for Car and Bike classes using super inheritance

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
-class Bike
-  def initialize
+class Vehicle
+  def initialize()
     @speed = 0
     @direction = "north"
   end
@@ -15,21 +15,39 @@ class Bike
   def turn(new_direction)
     @direction = new_direction
   end
+end
+
+class Bike < Vehicle
+  def initialize(input_options)
+    super()
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+    @color = input_options[:color]
+  end
 
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-class Car < Bike
+class Car < Vehicle
+  def initialize(input_options)
+    super()
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @color = input_options[:color]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-bike = Bike.new
-car = Car.new
+bike = Bike.new({ type: "cruiser", weight: "heavy", color: "red" })
+car = Car.new({ make: "nissan", model: "frontier", color: "silver" })
 bike.accelerate
 car.accelerate
 bike.ring_bell
 car.honk_horn
+p bike
+p car
