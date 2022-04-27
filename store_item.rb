8b0e5b4ea7ea9866fcfce6d8f3a26f5cfaa27c2a
Brad Bell <brad.bb.bell@gmail.com>
2022-04-27 10:49:23 -0600

    Implements attr_accessor and input_options hash in Item class

--- store_item.rb
+++ store_item.rb
@@ -4,35 +4,39 @@
 # puts "My store has great food. #{item1[:name]}, #{item2[:name]} and #{item3[:name]}."
 
 class Item 
-  def initialize(input_name, input_price, input_flavor)
-    @name = input_name
-    @price = input_price
-    @flavor = input_flavor
+  
+  attr_accessor :name, :price, :flavor
+  # attr_writer :name, :price, :flavor
+
+  def initialize(input_options)
+    @name = input_options[:name]
+    @price = input_options[:price]
+    @flavor = input_options[:flavor]
   end
 
-  def name
-    @name
-  end
+  # def name
+  #   @name
+  # end
 
-  def name=(input_name)
-    @name = input_name
-  end
+  # def name=(input_name)
+  #   @name = input_name
+  # end
 
-  def price
-    @price
-  end
+  # def price
+  #   @price
+  # end
 
-  def price=(input_price)
-    @price = input_price
-  end
+  # def price=(input_price)
+  #   @price = input_price
+  # end
 
-  def flavor
-    @flavor
-  end
+  # def flavor
+  #   @flavor
+  # end
 
-  def flavor=(input_flavor)
-    @flavor = input_flavor
-  end
+  # def flavor=(input_flavor)
+  #   @flavor = input_flavor
+  # end
 
   def print_info
     puts "#{@flavor} #{@name} $#{@price}"
@@ -40,9 +44,9 @@ class Item
 
 end
 
-item1 = Item.new("doughnut", 1, "glazed")
-item2 = Item.new("muffin", 2, "lemon poppyseed")
-item3 = Item.new("pizza", 3, "pepperoni")
+item1 = Item.new({name: "doughnut", price: 1, flavor: "glazed"})
+item2 = Item.new({name: "muffin", price: 2, flavor: "lemon poppyseed"})
+item3 = Item.new({name: "pizza", price: 3, flavor: "pepperoni"})
 
 puts "My store has great food. #{item1.name}, #{item2.name} and #{item3.name}."
 puts "Menu:"
@@ -51,3 +55,4 @@ item2.flavor = "blueberry"
 item2.print_info
 item3.price = 3.5
 item3.print_info
+p item3
